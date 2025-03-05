da4ea45615be406481cd2606f9f587695c5f157e
Jeffato <jlb671@scarletmail.rutgers.edu>
2025-03-05 15:15:48 -0500

    Merge multiple constructors

--- src/receipt.py
+++ src/receipt.py
@@ -11,28 +11,26 @@ class Receipt:
     image_url : str
     id : int
     
-    # TODO: more input validation might be needed
-    def __init__(self, category : str, requestor : str, amount : str, date_purchase : datetime, description : str, submit_time : datetime):
+    # TODO: more input validation might be needed. Check best practices for type annotation, kinda messy
+    def __init__(self, category : str, requestor : str, amount, date_purchase, description : str, submit_time : datetime, image_url = None, id = None):
         self.category = category 
         self.requestor = requestor
         self.description = description
 
         #TODO: Check for non num or decimal point
-        amount = re.sub(r',', "", amount)
-        self.amount = round(float(amount), ndigits = 2)
-        
+        if isinstance(amount, str):
+            amount = re.sub(r',', "", amount)
+            self.amount = round(float(amount), ndigits = 2)
+        else:
+            self.amount = amount
+
         #TODO: Check for date format
-        self.date_purchase = datetime.strptime(date_purchase, "%Y-%m-%d").date()  # Convert to date object
-        self.submit_time = submit_time
-        id = None
-    
-    # Used for reading from full db
-    def __init__(self, category : str, requestor : str, amount : float, date_purchase : datetime, description : str, submit_time : datetime, image_url: str, id: int):
-        self.category = category 
-        self.requestor = requestor
-        self.description = description
-        self.amount = amount
-        self.date_purchase = date_purchase
+        if isinstance(date_purchase, str):
+            # If it's a string, convert to datetime object
+            self.date_purchase = datetime.strptime(date_purchase, "%Y-%m-%d").date()
+        else:
+            self.date_purchase = date_purchase
+
         self.submit_time = submit_time
         self.image_url = image_url
         self.id = id
